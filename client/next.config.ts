ccaee5457e086b7c7f869e3169b512cba32943a7
buriihenry <buriihenry@gmail.com>
2025-07-07 12:59:19 +0300

    Add client folder as normal directory

---
+++ client/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {
+    turbo: false, // Disable Turbopack
+  },
+};
+
+export default nextConfig;
